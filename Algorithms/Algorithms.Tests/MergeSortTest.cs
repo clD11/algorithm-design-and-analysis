b9ac07e2dbf46be2abe86fd552932a76b50111a5
clD11 <clD11@fake.com>
2017-02-13 22:02:08 +0000

    first code commit

---
+++ Algorithms/Algorithms.Tests/MergeSortTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class MergeSortTest
+    {
+        private MergeSort mergeSort;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mergeSort = new MergeSort();
+        }
+
+        [TestMethod]
+        public void ShouldReturnSortedArray()
+        {
+            var unsorted = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var actual = mergeSort.Sort(unsorted);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 2 }, new int[] { 1 }, new int[] { 1, 2 })]
+        [DataRow(new int[] { 2, 3 }, new int[] { 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 1, 2, 3, 4, 5, 6 })]        
+        public void ShouldMergeAndSortArray(int[] left, int[] right, int[] expected)
+        {
+            var actual = mergeSort.Merge(left, right);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/MergeSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class MergeSort
+    {
+        public int[] Sort(int[] array)
+        {
+            if (array.Length <= 1)
+            {
+                return array;
+            }
+
+            var middle = array.Length / 2;
+
+            var left = new int[middle];
+            var right = new int[array.Length - middle];
+
+            Array.Copy(array, left, middle);
+            Array.Copy(array, middle, right, 0, right.Length);
+
+            left = Sort(left);
+            right = Sort(right);
+
+            return Merge(left, right);
+        }
+
+        public int[] Merge(int[] left, int[] right)
+        {
+            var merged = new int[left.Length + right.Length];
+
+            var i = 0;
+            var j = 0;
+
+            for (var k = 0; k < merged.Length; k++)
+            {
+                if (left[i] < right[j])
+                {
+                    merged[k] = left[i++];
+                }
+                else
+                {
+                    if (right[j] < left[i])
+                    {
+                        merged[k] = right[j++];
+                    }
+                }
+
+                // Check end cases
+
+                if (j >= right.Length)
+                {
+                    ++k;
+
+                    for (var e = i; e < left.Length; e++)
+                    {
+                        merged[k++] = left[e];
+                    }
+
+                    return merged;
+                }
+
+                if (i >= left.Length)
+                {
+                    ++k;
+
+                    for (var e = j; e < right.Length; e++)
+                    {
+                        merged[k++] = right[e];
+                    }
+
+                    return merged;
+                }
+            }
+
+            return merged;
+        }
+    }
+}
