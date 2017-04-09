71c5e6c6fbbc10e90fd2829b1fde0f326e30bfa1
clD11 <clD11@fake.com>
2017-04-09 23:14:50 +0100

    QuickSort implementation

---
+++ Algorithms/Algorithms.Tests/QuicksortTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        private QuickSort testee = new QuickSort();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            testee = new QuickSort();
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 5, 4, 3, 2, 1 }, new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 12, 4, 6, 2, 1 }, new int[] { 1, 2, 4, 6, 12 })]
+        [DataRow(new int[] { 2, 2, 1, 1, 3 }, new int[] { 1, 1, 2, 2, 3 })]
+        public void TestQuickSortShouldReturnSortedArray(int[] actual, int[] expected)
+        {
+            testee.Sort(actual, 0, actual.Length - 1);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/QuickSort.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms
+{
+    public class QuickSort
+    {
+        public void Sort(int[] arr, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = partition(arr, p, r);
+                Sort(arr, p, q - 1);
+                Sort(arr, q + 1, r);
+            }
+        }
+
+        public int partition(int[] arr, int p, int r)
+        {
+            int x = arr[r];
+            int i = p - 1;
+
+            for (var j = p; j <= r - 1; j++)
+            {
+                if (arr[j] <= x)
+                {
+                    i = i + 1;
+                    swap(ref arr, i, j);
+                }                
+            }
+
+            swap(ref arr, i + 1, r);
+
+            return i + 1;
+        }
+
+        public void swap(ref int[] arr, int i, int j)
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp; 
+        }
+    }
+}
